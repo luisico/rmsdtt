c1ba81e758bbf333940910e701ea7f49f9f3b143
Luis Gracia <lgraval@gmail.com>
2010-08-02 19:55:12 +0000

    Several changes
    - Update rmsdtt to version 3.0, including documentation.
    - Small costemic changes to html.

--- make-distrib.sh
+++ make-distrib.sh
@@ -7,15 +7,31 @@ plugin=rmsdtt
 dir=$plugin
 tar=$plugin-v$version.tgz
 
+files=(
+rmsdtt.tcl
+#iterativefit.tcl
+pkgIndex.tcl
+doc
+)
+
 # re-arrange documentation
 mkdir doc
-cp index.html rmsdtt_2.0.png doc
+cp index.html rmsdtt_3.0.png doc
 wget -q -O doc/vmdbackup.css http://physiology.med.cornell.edu/resources/physbio.css http://physiology.med.cornell.edu/faculty/hweinstein/vmdplugins/vmd.css
 chmod -R ugo+rX doc
 
+# Build list of files
+for f in ${files[@]}; do
+    if [ ! -r $f ]; then
+	echo "ERROR: File \"$dir/$f\" is not readable!"
+	exit 11
+    fi
+    dirfiles="$dirfiles $dir/$f"
+done
+
 # Compress
 cd ../
-tar zcvf $tar $dir/pkgIndex.tcl $dir/rmsdtt.tcl $dir/doc
+tar zcvf $tar $dirfiles
 mv $tar $dir/versions
 chmod 644 $dir/versions/$tar
 cd $dir
